f337a35c80e1a157cf2381f9036d5a712f6e438c
Shlok Mishra <shlokmishra08@tamu.edu>
2025-11-21 13:21:02 -0600

    Add vcov() method for autodiffr_fit objects

---
+++ R/vcov.R
@@ -0,0 +1,12 @@
+#' Extract Variance-Covariance Matrix from autodiffr_fit
+#'
+#' @param object An object of class `autodiffr_fit`
+#' @param ... Additional arguments (currently ignored)
+#'
+#' @return A variance-covariance matrix, or NULL if not available
+#'
+#' @export
+vcov.autodiffr_fit <- function(object, ...) {
+  object$vcov
+}
+
